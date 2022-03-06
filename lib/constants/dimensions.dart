44b8608ac4b15838246d535912dbaa777e3e336c
betty arega <bettyarega3@gmail.com>
2022-03-06 10:21:02 +0300

    making it reponsive

---
+++ lib/constants/dimensions.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+
+class Dimensions{
+  static double screenHeight = Get.context!.height;
+  static double screenWidth = Get.context!.width;
+
+  static double pageView = screenHeight/2.66;
+  static double pageViewContainer = screenHeight/3.84;
+  static double pageViewTextContainer = screenHeight/6.92;
+  static double listViewContainer = screenHeight/2.66;
+
+  static double height8 = screenHeight/86.5;
+  static double height12 = screenHeight/57.66;
+  static double height16 = screenHeight/43.25;
+  static double height25 = screenHeight/27.68;
+  static double height36 = screenHeight/19.22;
+
+  static double width8 = screenWidth/45.0;
+  static double width12 = screenWidth/30.0;
+  static double width16 = screenWidth/22.5;
+  static double width25 = screenWidth/14.4;
+  static double width36 = screenWidth/10.0;
+
+  static double font16 = screenHeight/43.25;
+
+  static double radius16 = screenHeight/43.25;
+  static double radius12 = screenHeight/57.66;
+  static double radius25 = screenHeight/27.68;
+  
+  
+  static double iconSize20 = screenHeight/34.6;
+  static double iconSize12 = screenHeight/57.66;
+
+
+}
+ 
