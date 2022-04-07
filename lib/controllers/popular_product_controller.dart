542f727b4c121f0190f73b42778f47f0acbfd06f
betty arega <bettyarega3@gmail.com>
2022-04-07 10:22:32 +0300

    popular food controller updated to show items in cart

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -1,33 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:food_ordering_app/constants/colors.dart';
+import 'package:food_ordering_app/controllers/cart_controller.dart';
 import 'package:food_ordering_app/data/repository/popular_product_repo.dart';
+import 'package:food_ordering_app/models/cart.dart';
 import 'package:food_ordering_app/models/product.dart';
 import 'package:get/get.dart';
 
-class PopularProductController extends GetxController{
+class PopularProductController extends GetxController {
   final PopularProductRepo popularProductRepo;
 
   PopularProductController({required this.popularProductRepo});
 
   List<ProductModel> _popularProductList = [];
   List<ProductModel> get popularProductList => _popularProductList;
+  late CartController _cart;
 
   bool _isLoaded = false;
   bool get isLoaded => _isLoaded;
+  int _quantity = 0;
+  int get quantity => _quantity;
+  int _inCartItems = 0;
+  int get inCartItems => _inCartItems + _quantity;
 
-  Future<void> getPopularProductList() async{
+  Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
-    if(response.statusCode ==200){
-      
+    if (response.statusCode == 200) {
       print('got products');
       _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).products);
       _isLoaded = true;
       //print(_popularProductList);
       update();
-    }else{
+    } else {}
+  }
 
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = checkQuantity(_quantity + 1);
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
+    }
+    update();
+  }
+
+  int checkQuantity(int quantity) {
+    if (inCartItems + quantity < 0) {
+      Get.snackbar("Item count", "can't reduce more.",
+          backgroundColor: AppColors.mainColor, colorText: Colors.white);
+
+          if(inCartItems > 0){
+            _quantity = - _inCartItems;
+            return _quantity; 
+          }
+      return 0;
+    } else if (inCartItems + quantity > 20) {
+      Get.snackbar("Item count", "can't order more than 20 on one product.",
+          backgroundColor: AppColors.mainColor, colorText: Colors.white);
+      return 20;
+    } else {
+      return quantity;
     }
   }
 
+  void initProduct(ProductModel product, CartController cart) {
+    _quantity = 0;
+    _inCartItems = 0;
+    _cart = cart;
+    var exist = false;
+    exist = _cart.existIncart(product);
 
+    if (exist) {
+      _inCartItems = _cart.getQuantity(product);
+    }
+  }
 
+  void addItem(ProductModel product) {
+    // if(quantity > 0){
+    _cart.addItem(product, _quantity);
+    _quantity = 0;
+    _inCartItems = _cart.getQuantity(product);
+    // }else{
+    //   Get.snackbar("item count", "minimum 1 amount needed");
+    //}
+    update();
+  }
+
+  int get totalItems {
+    return _cart.totalItems;
+  }
+
+  List<CartModel > get getItems{
+    return _cart.getItems;
+  }
 }
